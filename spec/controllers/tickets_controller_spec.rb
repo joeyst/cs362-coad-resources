a210a14f81d128f146937a5fc618ebf4b5611746
joeyst <joeystenbeck@gmail.com>
2022-03-01 11:37:47 -0800

    tickets_controller_spec: add unit test for :show

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -2,4 +2,13 @@ require 'rails_helper'
 
 RSpec.describe TicketsController, type: :controller do
 
+  describe "a non-logged in person" do
+
+    it "redirects to the sign-in screen" do
+      get :show, params: {id: 'FAKE'}
+      expect(response).to redirect_to(new_user_session_path)
+    end
+
+  end
+
 end
