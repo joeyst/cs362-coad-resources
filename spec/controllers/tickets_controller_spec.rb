267f5a063022a4b487aa124dda26e6da2f3cbb9f
joeyst <joeystenbeck@gmail.com>
2022-03-03 11:20:59 -0800

    tickets_controller_spec: add unit test for logged-in user

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe TicketsController, type: :controller do
 
   describe "a non-logged in person" do
 
-    it "redirects to the sign-in screen" do
+    it "redirects to dashboard" do
       get :show, params: {id: 'FAKE'}
       expect(response).to redirect_to(dashboard_path)
       get :close, params: {id: 'FAKE'}
@@ -12,6 +12,15 @@ RSpec.describe TicketsController, type: :controller do
       post :capture, params: {id: 'FAKE'}
       expect(response).to redirect_to(dashboard_path)
       post :release, params: {id: 'FAKE'}
+      expect(response).to redirect_to(dashboard_path)
+    end
+
+  end
+
+  describe "a logged-in user" do
+
+    it "redirects to dashboard_path" do
+      delete :destroy, params: {id: 'FAKE'}
       expect(response).to redirect_to(dashboard_path)
     end
 
