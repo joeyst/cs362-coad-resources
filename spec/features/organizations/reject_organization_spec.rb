f4c5c933535d3a78feb8e75e5d26fa8077388cb4
joeyst <joeystenbeck@gmail.com>
2022-03-11 18:10:29 -0800

    reject_organization_spec: add feature test for an admin rejecting a pending organization

--- spec/features/organizations/reject_organization_spec.rb
+++ spec/features/organizations/reject_organization_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'Rejecting an organization', type: :feature do
+  let(:admin) { create(:user, :admin)}
+  
+  before do
+    admin.confirm
+    log_in_as(admin)
+  end
 
+  it "allows an admin to reject a pending organization" do
+    create(:organization)
+    visit dashboard_path
+    click_on 'Organizations'
+    click_on 'Pending'
+    click_on 'Review'
+    fill_in 'Rejection Reason', with: 'Fake Rejection Reason'
+    click_on 'Reject'
+    expect(page).to have_text('has been rejected.')
+  end
 end
