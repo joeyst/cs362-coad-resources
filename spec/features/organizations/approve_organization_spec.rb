85060b206f9d85e2bc65513a53a8af178757937b
joeyst <joeystenbeck@gmail.com>
2022-03-11 17:54:16 -0800

    approve_organization_spec: add feature test for an admin approving an organization

--- spec/features/organizations/approve_organization_spec.rb
+++ spec/features/organizations/approve_organization_spec.rb
@@ -2,4 +2,21 @@ require 'rails_helper'
 
 RSpec.describe 'Approving an organization', type: :feature do
 
+  let(:admin) { create(:user, :admin)}
+
+  before do
+    admin.confirm
+    log_in_as(admin)
+  end
+
+  it "allows admin to approve an organization" do
+    create(:organization)
+    visit dashboard_path
+    expect(current_path).to eql(dashboard_path)
+    click_on 'Organizations'
+    click_on 'Pending'
+    click_on 'Review'
+    click_on 'Approve'
+    expect(page).to have_text('has been approved.')
+  end
 end
