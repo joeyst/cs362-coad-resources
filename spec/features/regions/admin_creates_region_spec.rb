685a1ca538cd8fa14f739ed512d815e0ea443559
Luke Fernandez <luke.matthew.fernandez@gmail.com>
2022-03-13 12:52:40 -0700

    admin_deletes_region_spec: move admin login setup to correct file

--- spec/features/regions/admin_creates_region_spec.rb
+++ spec/features/regions/admin_creates_region_spec.rb
@@ -2,11 +2,4 @@ require 'rails_helper'
 
 RSpec.describe 'Creating a Region', type: :feature do
 
-  let(:admin) { create(:admin) }
-
-  before do
-    admin.confirm
-    log_in_as(admin)
-  end
-
 end
--- spec/features/regions/admin_deletes_region_spec.rb
+++ spec/features/regions/admin_deletes_region_spec.rb
@@ -2,4 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'Deleting a Region', type: :feature do
 
+  let(:admin) { create(:admin) }
+
+  before do
+    admin.confirm
+    log_in_as(admin)
+  end
+
+  it "allows an admin to delete a region" do
+  end
 end
