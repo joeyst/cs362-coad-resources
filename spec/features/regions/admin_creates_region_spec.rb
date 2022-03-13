da67684272713566445492811b488e6f36e9b2f3
Luke Fernandez <luke.matthew.fernandez@gmail.com>
2022-03-13 12:51:21 -0700

    admin_creates_region_spec: define admin login setup

--- spec/features/regions/admin_creates_region_spec.rb
+++ spec/features/regions/admin_creates_region_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating a Region', type: :feature do
-  
+
+  let(:admin) { create(:admin) }
+
+  before do
+    admin.confirm
+    log_in_as(admin)
+  end
+
 end
