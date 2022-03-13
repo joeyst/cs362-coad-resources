b5ef8ca150df40784c263116ccc30edd5c21d656
Luke Fernandez <luke.matthew.fernandez@gmail.com>
2022-03-13 12:59:16 -0700

    admin_deletes_region_spec: feature test region deletion

--- spec/features/regions/admin_deletes_region_spec.rb
+++ spec/features/regions/admin_deletes_region_spec.rb
@@ -10,5 +10,14 @@ RSpec.describe 'Deleting a Region', type: :feature do
   end
 
   it "allows an admin to delete a region" do
+    region_name = 'fake region'
+
+    visit new_region_path
+    fill_in('region_name', :with => region_name)
+    click_on 'Add Region'
+
+    visit region_path(:id => 1)
+    click_on 'Delete'
+    expect(page).to have_content('Region ' + region_name + ' was deleted.')
   end
 end
