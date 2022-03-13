27796758121cbc6d3a0ed1517682c521acb9b2bf
joeyst <joeystenbeck@gmail.com>
2022-03-13 11:25:02 -0700

    application_helper_spec: add describe block for full_title method

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
-  it "uses base_title if page_title is empty" do
-    expect(full_title).to eql('Disaster Resource Network')
-  end
+  describe 'full_title method' do
+    it "uses base_title if page_title is empty" do
+      expect(full_title).to eql('Disaster Resource Network')
+    end
 
-  it "uses base_title and page_title if page_title isn't empty" do
-    expect(full_title('Fake Page Title')).to eql('Fake Page Title | Disaster Resource Network')
+    it "uses base_title and page_title if page_title isn't empty" do
+      expect(full_title('Fake Page Title')).to eql('Fake Page Title | Disaster Resource Network')
+    end
   end
 end
