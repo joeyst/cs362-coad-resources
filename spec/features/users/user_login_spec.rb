08057a4dba4796cc6924b2dd6c34e7d6d590f46e
Luke Fernandez <luke.matthew.fernandez@gmail.com>
2022-03-13 13:23:59 -0700

    user_login_spec: test login with valid credentials

--- spec/features/users/user_login_spec.rb
+++ spec/features/users/user_login_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe 'Logging in', type: :feature do
+  
+  let (:user) { create(:user) }
+  
+  before do
+    user.confirm
+  end
 
+  it "allows a guest to log in" do
+    visit login_path
+
+    fill_in('user_email', :with => user.email)
+    fill_in('user_password', :with => user.password)
+    check('user_remember_me')
+
+    click_on('Sign in')
+
+    expect(page).to have_content('Signed in successfully.')
+  end
 end
